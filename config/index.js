7db53d6e3663ccec077c0171e6dc8a4e965adb88
Joanna Kao <joanna.kao@ft.com>
2017-02-15 18:33:36 -0500

    add id to each card so we can link to them (use tweet id of first tweet)

--- config/index.js
+++ config/index.js
@@ -25,15 +25,18 @@ export default async function() {
     const results = await Promise.all(data.map(async function(tweet) {
       tweet.tweetMetadata = await Promise.all(tweet.tweeturl.split(',').map(async function(tweet) {
         let tweetEmbedCode = '';
+        let tweetId = '';
         try {
           const response = await axios(`https://publish.twitter.com/oembed?url=${tweet}`);
           tweetEmbedCode = response.data.html;
+          tweetId = response.data.url.split('https://twitter.com/realDonaldTrump/status/')[1];
         } catch (e) {
           console.log('Error getting tweet embed code');
         }
 
         return {
           tweetURL: tweet,
+          tweetId,
           tweetEmbedCode,
         }
       }));
